42d32a8785a96f2aa6f6f12b9337d13664fd384b
Emmanueloz <davidozuna84@hotmail.com>
2023-11-24 18:50:49 -0600

    Estructuras de control

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask import Flask, request, make_response, redirect, render_template
 def create_app():
     app = Flask(__name__)
 
+    todos = ['TODO 1', 'TODO 2', 'TODO 3']
+
     @app.route("/")
     def index():
         user_ip = request.remote_addr
@@ -14,6 +16,11 @@ def create_app():
     @app.route("/hello")
     def hello():
         user_ip = request.cookies.get('user_ip')
-        return render_template('hello.html', user_ip=user_ip)
+        print(todos)
+        context = {
+            "user_ip": user_ip,
+            "todos": todos
+        }
+        return render_template('hello.html', **context)
 
     return app
